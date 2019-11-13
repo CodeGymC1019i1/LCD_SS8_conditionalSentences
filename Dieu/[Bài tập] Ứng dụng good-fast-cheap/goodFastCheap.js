6b0056dc935180f158326162124cc319c027a87f
greendvn <greendvn@gmail.com>
2019-11-13 22:50:14 +0700

    up lai good- fast- cheap

--- Dieu/[Bài tập] Ứng dụng good-fast-cheap/goodFastCheap.js
+++ Dieu/[Bài tập] Ứng dụng good-fast-cheap/goodFastCheap.js
@@ -1,34 +1,28 @@
 
-function goodFastCheap1() {
+function goodFastCheap(id) {
     let good = document.getElementById("good");
     let fast = document.getElementById("fast");
     let cheap = document.getElementById("cheap");
-    if (good.checked ) {
-        if (fast.checked) {
-            cheap.checked = false;
-        }
+    switch (id) {
+        case "good":
+            if (good.checked ) {
+                if (fast.checked) {
+                    cheap.checked = false;
+                }
+            }
+        case "fast":
+            if (fast.checked ) {
+                if (cheap.checked)
+                    good.checked = false;
+            }
+        case "cheap":
+            if (cheap.checked ) {
+                if (good.checked) {
+                    fast.checked = false;
+                }
+            }
     }
     document.getElementById("cheap").checked = cheap.checked;
-}
-function goodFastCheap2() {
-    let good = document.getElementById("good");
-    let fast = document.getElementById("fast");
-    let cheap = document.getElementById("cheap");
-    if (fast.checked ) {
-        if (cheap.checked)
-            good.checked = false;
-    }
     document.getElementById("good").checked = good.checked;
-}
-function goodFastCheap3() {
-    let good = document.getElementById("good");
-    let fast = document.getElementById("fast");
-    let cheap = document.getElementById("cheap");
-
-    if (cheap.checked ) {
-        if (good.checked) {
-            fast.checked = false;
-        }
-    }
     document.getElementById("fast").checked = fast.checked;
 }
